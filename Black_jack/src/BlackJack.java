c267c7df3679e75aec49a4ce765975ee2862af3f
loo919239 <loo919239@naver.com>
2023-12-06 16:41:49 +0900

    블랙잭

--- Black_jack/src/BlackJack.java
+++ Black_jack/src/BlackJack.java
@@ -1,6 +1,22 @@
 
 public class BlackJack {
+	public static void main() {
+		int[] card = new int[11];
+		int[] patturn = new int[3];
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+				String cards;
+				if (i == 1) cards = "A";	
+				else if (i == 11) cards = "K";
+				else if (i == 12) cards = "Q";
+				else if (i == 13) cards = "J";
+			}
+		}
+	}
 	public static void main(String[] args) {
-
+		
+		
+		
 	}
 }
